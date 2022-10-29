d30066bdc6ca01f87aab325ab9d62380349a58a4
Andrew Subotin <subotinandrey5@gmail.com>
2022-10-29 12:30:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='address_book_bot',
     version='1',
-    description='Very useful code',
-    url='http://github.com/dummy_user/useful',
-    author='Flying Circus',
-    author_email='flyingcircus@example.com',
+    description='Bot for saving contacts in the Address Book.',
+    url='https://github.com/Andrew-157/address_book_bot',
+    author='Andrew-157',
+    author_email='subotinandrey5@g,ail.com',
     license='MIT',
     packages=find_namespace_packages()
 )
